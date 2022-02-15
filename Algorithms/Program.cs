11f2562134e1cb9f631a91de34f0f074b7afc385
Sachith Jayasinghe <SachithJ@99x.io>
2022-02-15 06:07:49 +0530

    Depth of a binary tree

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithms.LinkListMiddleNode;
+using Algorithms.Trees;
 using System;
 using System.Collections.Generic;
 
@@ -9,7 +10,38 @@ namespace Algorithms
         static void Main(string[] args)
         {
             // find the middle of a link list
-            TortoiseAndHare.RunSample();
+            //TortoiseAndHare.RunSample();
+            DepthOfTree.RunSample();
+
+            //solve(4, "A", "B", "C");
+
+            //recursion(4);
+        }
+
+
+        public static void solve(int n, string start, string auxiliary, string end)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(start + " -> " + end);
+            }
+            else
+            {
+                solve(n - 1, start, end, auxiliary);
+                Console.WriteLine(start + " -> " + end);
+                solve(n - 1, auxiliary, start, end);
+            }
+        }
+
+
+        public static void recursion(int x,string t =null)
+        {
+            if (x == 0)
+                return;
+            Console.WriteLine(x+ t);
+            x--;
+            recursion(x);
+            recursion(x, "'");
         }
     }
 }
